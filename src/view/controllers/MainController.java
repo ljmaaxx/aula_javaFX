175aca5171799843f24b6d1ced4a0e80219a316c
Thiago Gomes <thiago123789@gmail.com>
2017-08-10 17:39:12 -0300

    modificação controller

--- src/view/controllers/MainController.java
+++ src/view/controllers/MainController.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class MainController{
@@ -33,50 +34,59 @@ public class MainController{
 		this.sis = Sistema.getInstance();
 		this.user = "thiago";
 		this.pass = "123456";
+		
+		this.passwordField.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+                login();
+            }
+		});
 		this.loginButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Stage stage = null;
-				Parent root = null;
-				boolean loginOk = false;
-				try{
-					if(event.getSource() == loginButton){
-						if(userField.getText().equals(user)){
-							if(passwordField.getText().equals(pass)){
-								stage = (Stage) loginButton.getScene().getWindow();
-								root = (Parent) FXMLLoader.load(getClass().getResource("/view/OverviewTemplate.fxml"));
-								loginOk = true;
-							}else{
-								Alert alert = new Alert(AlertType.ERROR);
-								alert.setTitle("Falha de Login");
-								alert.setHeaderText("Informações inválidas");
-								alert.setContentText("Senha incorreta");
-								alert.showAndWait();
-							}
-						}else{
-							Alert alert = new Alert(AlertType.ERROR);
-							alert.setTitle("Falha de Login");
-							alert.setHeaderText("Informações inválidas");
-							alert.setContentText("Usuário não encontrado");
-							alert.showAndWait();
-						}
-					}
-					if(loginOk){
-						Scene scene = new Scene(root);
-						stage.setScene(scene);
-						String tituloAtual = stage.getTitle();
-						stage.setTitle(tituloAtual +" - "+ (""+user.charAt(0)).toUpperCase() + user.substring(1, user.length()));
-						stage.setResizable(true);
-						sis.changeStage(stage);
-					}
-				}catch(IOException e){
-					e.printStackTrace();
-				}
+				login();
 			}
 		});
 		
 		
 	}
+	
+	private void login(){
+		Stage stage = null;
+		Parent root = null;
+		boolean loginOk = false;
+		try{
+			if(userField.getText().equals(user)){
+				if(passwordField.getText().equals(pass)){
+					stage = (Stage) loginButton.getScene().getWindow();
+					root = (Parent) FXMLLoader.load(getClass().getResource("/view/OverviewTemplate.fxml"));
+					loginOk = true;
+				}else{
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Falha de Login");
+					alert.setHeaderText("Informações inválidas");
+					alert.setContentText("Senha incorreta");
+					alert.showAndWait();
+				}
+			}else{
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Falha de Login");
+				alert.setHeaderText("Informações inválidas");
+				alert.setContentText("Usuário não encontrado");
+				alert.showAndWait();
+			}
+			if(loginOk){
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				String tituloAtual = stage.getTitle();
+				stage.setTitle(tituloAtual +" - "+ (""+user.charAt(0)).toUpperCase() + user.substring(1, user.length()));
+				stage.setResizable(true);
+				sis.changeStage(stage);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 	
 	private void overViewApp(){
 		
